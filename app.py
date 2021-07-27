ec5d5d654d24b3eff266648676152c6eae96f011
www980130 <www980130@naver.com>
2021-07-27 09:48:18 +0900

    First Commit1

---
+++ app.py
@@ -0,0 +1,34 @@
+#flask 라이브러리 안에 Flask 라는 객체 존재
+from flask import Flask, render_template, request
+from data import Articles
+
+app = Flask(__name__) #__name__이라는 내장변수를 받아 새로운 instance인 app 생성
+app.debug = True #파일 저장할 때마다 서버 restart하려면 debug 설정 추가
+
+@app.route('/', methods=['GET', 'POST']) # @: decorate
+def hello_world(): #함수 생성
+    # return 'Hello World!'
+    # return render_template('index.html', data = "안녕하세요 김태경 입니다.")
+    
+    articles = Articles() #articles는 리스트
+    # print(articles)
+    
+    # return render_template('index.html', data = "Main Page")
+
+    for i in articles:
+        print(i['title'])
+    return render_template('index.html', articles = articles)
+
+@app.route('/<id>/article', methods=['GET', 'POST'])
+def detail(id):
+    if request.method == 'GET':
+        articles = Articles()
+        print(articles[int(id)-1])
+        return render_template('detail.html', article = articles[int(id)-1])
+
+
+
+#내장변수가 name이면 다음 함수를 실행시켜라
+if __name__ == '__main__':
+    app.run(port=5000)
+
